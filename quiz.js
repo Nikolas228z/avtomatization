50f418aa8bbe9f3d8536b79481e707fa0e362604
cobtahacademy16 <cobtahacademy16@gmail.com>
2025-12-15 16:59:39 +0200

    #G05PROJ-19 added h1 and buttons, id for them and first const in js

--- quiz.js
+++ quiz.js
@@ -1,24 +1,30 @@
-let score = 0;
-let answered = 0;
-const totalQuestions = 5;
+let currentQuestion = 0;
+let answ1 = 0;
+let question = document.getElementById("question");
+let answer1 = document.getElementById("answer1");
 
-function answer(question, isCorrect) {
-    // 1. Приховуємо кнопки поточного питання
-    document.getElementById("q" + question).style.display = "none";
+const questions = [
+    { id: 1, name: "question1", answer1: "a1", answer2: "a2", correctAnswer: 1, answer3: "a3", answer4: "a4" },
+    { id: 2, name: "question2", answer1: "a1", answer2: "a2", correctAnswer: 2, answer3: "a3", answer4: "a4" },
+    { id: 2, name: "question3", answer1: "a1", answer2: "a2", correctAnswer: 3, answer3: "a3", answer4: "a4" }
+];
+next();
+function check(button, correct) {
+    if (questions[currentQuestion].correctAnswer == correct) {
+        console.log(correct);
 
-    // 2. Показуємо повідомлення
-    document.getElementById("msg" + question).style.display = "block";
-
-    // 3. Підрахунок балів
-    if (isCorrect) score++;
-    else score--; // або можна не мінусувати
-
-    answered++;
-
-    // 4. Показуємо результат після всіх питань
-    if (answered === totalQuestions) {
-        document.getElementById("result").innerText =
-            "Правильних відповідей: " + score +
-            "\nНеправильних: " + (totalQuestions - score);
+        button.style.backgroundColor = "green";
     }
+
+
+        currentQuestion = currentQuestion + 1;
+}
+
+function next() {
+    question.textContent = questions[currentQuestion].name;
+    answer1.textContent = questions[currentQuestion].answer1;
+    answer2.textContent = questions[currentQuestion].answer2;
+    answer3.textContent = questions[currentQuestion].answer3;
+    answer4.textContent = questions[currentQuestion].answer4;
+
 }
