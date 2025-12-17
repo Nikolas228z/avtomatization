ed201a2b5e5f2ee0d49853b67bcef0d4817c6574
cobtahacademy16 <cobtahacademy16@gmail.com>
2025-12-17 17:06:03 +0200

    #G05PROJ-23 timer after every answer, button are disabled when someone pressed it.

--- quiz.js
+++ quiz.js
@@ -1,8 +1,9 @@
 let currentQuestion = 0;
-let answ1 = 0;
 let question = document.getElementById("question");
 let answer1 = document.getElementById("answer1");
-
+let answer2 = document.getElementById("answer2");
+let answer3 = document.getElementById("answer3");
+let answer4 = document.getElementById("answer4");
 const questions = [
     { id: 1, name: "question1", answer1: "a1", answer2: "a2", correctAnswer: 1, answer3: "a3", answer4: "a4" },
     { id: 2, name: "question2", answer1: "a1", answer2: "a2", correctAnswer: 2, answer3: "a3", answer4: "a4" },
@@ -11,16 +12,27 @@ const questions = [
 ];
 next();
 function check(button, correct) {
+    answer1.disabled = true;
+    answer2.disabled = true;
+    answer3.disabled = true;
+    answer4.disabled = true;
     if (questions[currentQuestion].correctAnswer == correct) {
         console.log(correct);
         button.style.backgroundColor = "green";
+    } else {
+        button.style.backgroundColor = "red";
     }
     currentQuestion = currentQuestion + 1;
-}
-function next() {
-    question.textContent = questions[currentQuestion].name;
-    answer1.textContent = questions[currentQuestion].answer1;
-    answer2.textContent = questions[currentQuestion].answer2;
-    answer3.textContent = questions[currentQuestion].answer3;
-    answer4.textContent = questions[currentQuestion].answer4;
+    let timer = setInterval(() => {
+        question.textContent = questions[currentQuestion].name;
+        answer1.textContent = questions[currentQuestion].answer1;
+        answer2.textContent = questions[currentQuestion].answer2;
+        answer3.textContent = questions[currentQuestion].answer3;
+        answer4.textContent = questions[currentQuestion].answer4;
+        button.style.backgroundColor = "white";
+        answer1.disabled = false;
+        answer2.disabled = false;
+        answer3.disabled = false;
+        answer4.disabled = false;
+    }, 3000);
 }
