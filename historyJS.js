e6f61c3e63268c722e6b03dfb18a5270e1e43360
Nikolas228z <cobtahacademy17@gmail.com>
2025-12-22 17:02:59 +0200

    #G05PROJ-20 fix animation for text

---
+++ historyJS.js
@@ -0,0 +1,10 @@
+function ChangingLeft(){
+ let changLeft = document.getElementById("pageOne");
+    let taxt = document.getElementById("titleOfbook");
+    taxt.style.display="none";
+    changLeft.classList.add("turned");
+}
+function changingRight(){
+    let changRight = document.getElementById("pageTwo");
+    changRight.classList.add("turnedTwo");
+}
