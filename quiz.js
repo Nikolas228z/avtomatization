346832ee31c1ffe9c8dcf2419823c7e518aab892
cobtahacademy16 <cobtahacademy16@gmail.com>
2025-12-23 15:33:50 +0200

    #G05PROJ-24 added a line that is changing after every question

--- quiz.js
+++ quiz.js
@@ -11,6 +11,9 @@ let score = 0;
 let p = document.getElementById("result");
 /*let next = document.getElementById("next");*/
 let buttons = div.getElementsByTagName("button");
+let div2 = document.getElementById("div2");
+let green = document.getElementById("green");
+let red = document.getElementById("red");
 console.log(buttons);
 const questions = [
     { id: 1, name: "question1", answer1: "a1", answer2: "a2", correctAnswer: 1, answer3: "a3", answer4: "a4" },
@@ -18,6 +21,8 @@ const questions = [
     { id: 3, name: "question3", answer1: "a1", answer2: "a2", correctAnswer: 3, answer3: "a3", answer4: "a4" },
     { id: 4, name: "question4", answer1: "a1", answer2: "a2", correctAnswer: 4, answer3: "a3", answer4: "a4" }
 ];
+let allquestion = 100 / questions.length;
+console.log(allquestion);
 function check(button, correct) {
     answer1.disabled = true;
     answer2.disabled = true;
@@ -32,23 +37,31 @@ function check(button, correct) {
         buttons[questions[currentQuestion].correctAnswer - 1].style.backgroundColor = "green";
     }
     currentQuestion = currentQuestion + 1;
+    div2.style.width = allquestion * currentQuestion + "%";
+
     if (currentQuestion == questions.length) {
         div.style.display = "none";
+        let forGreen = score * allquestion;
+        console.log(forGreen);
+        let forRed = question.length - score;
+        console.log(forRed);
         p.textContent = "Your score:" + score + " Answered correct:" + score;
+    } else{
+
+        timer = setInterval(() => {
+            question.textContent = questions[currentQuestion].name;
+            answer1.textContent = questions[currentQuestion].answer1;
+            answer2.textContent = questions[currentQuestion].answer2;
+            answer3.textContent = questions[currentQuestion].answer3;
+            answer4.textContent = questions[currentQuestion].answer4;
+            chc();
+            answer1.disabled = false;
+            answer2.disabled = false;
+            answer3.disabled = false;
+            answer4.disabled = false;
+            clearInterval(timer);
+        }, 3000);
     }
-    timer = setInterval(() => {
-        question.textContent = questions[currentQuestion].name;
-        answer1.textContent = questions[currentQuestion].answer1;
-        answer2.textContent = questions[currentQuestion].answer2;
-        answer3.textContent = questions[currentQuestion].answer3;
-        answer4.textContent = questions[currentQuestion].answer4;
-        chc();
-        answer1.disabled = false;
-        answer2.disabled = false;
-        answer3.disabled = false;
-        answer4.disabled = false;
-        clearInterval(timer);
-    }, 3000);
 }
 function chc() {
     for (i = 0; i < buttons.length; i = i + 1) {
