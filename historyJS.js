89bf86a46ac66162c20b3a8672a411814aa43766
Nikolas228z <cobtahacademy17@gmail.com>
2025-12-23 15:35:35 +0200

    #G05PROJ-20 add new workPage

--- historyJS.js
+++ historyJS.js
@@ -1,10 +1,12 @@
 function ChangingLeft(){
  let changLeft = document.getElementById("pageOne");
     let taxt = document.getElementById("titleOfbook");
-    taxt.style.display="none";
+    taxt.classList.add("titleOfBook");
     changLeft.classList.add("turned");
 }
 function changingRight(){
     let changRight = document.getElementById("pageTwo");
+     let InSecPage = document.getElementById("allTextInSecPage");
+    InSecPage.classList.add("titleOfBook");
     changRight.classList.add("turnedTwo");
 }
