a8f6469251c3d928c47bf856a3859a79ac915eb2
Nikolas228z <cobtahacademy17@gmail.com>
2025-12-30 17:34:52 +0200

    #G05PROJ-20 add animation of returne page

--- historyJS.js
+++ historyJS.js
@@ -1,12 +1,37 @@
-function ChangingLeft(){
+let butonOne = document.getElementById("pageOne");
+let butonTwo = document.getElementById("pageTwo");
+butonOne.addEventListener("click",  e => {
+    ChangingLeft(e.target);
+});
+butonTwo.addEventListener("click", ChangingLeft);
+
+
+
+
+
+function ChangingLeft(e1){
  let changLeft = document.getElementById("pageOne");
     let taxt = document.getElementById("titleOfbook");
     taxt.classList.add("titleOfBook");
     changLeft.classList.add("turned");
+    e1.removeEventListener("click", ChangingLeft);
+    e1.addEventListener("click", forChangeInRight);
 }
-function changingRight(){
+function changingRight(n){
     let changRight = document.getElementById("pageTwo");
+   // let changRightTwo = document.getElementById("pageTree");
      let InSecPage = document.getElementById("allTextInSecPage");
+  //   let InThirtPage = document.getElementById("allTextInThirtPage");
     InSecPage.classList.add("titleOfBook");
+  //  InThirtPage.classList.add("titleOfBook");
     changRight.classList.add("turnedTwo");
+   // changRightTwo.classList.add("turnedTwo");
+}
+function forChangeInRight(){
+    let changLeft = document.getElementById("pageOne");
+    let taxt = document.getElementById("titleOfbook");
+    taxt.classList.add("titleOfBook");
+    changLeft.classList.remove("turned");
+    taxt.classList.remove("titleOfBook");
+    changLeft.classList.add("returnedd");
 }
